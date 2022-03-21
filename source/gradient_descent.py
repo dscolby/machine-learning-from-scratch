cc4e058b35fccf46eca300286b996634c0930ef0
Darren Colby <73398189+dscolby@users.noreply.github.com>
2022-03-21 00:32:09 -0400

    Added stochastic gradient descent

--- source/gradient_descent.py
+++ source/gradient_descent.py
@@ -1,14 +1,15 @@
 # Author: Darren Colby
-# Date: 3/15/2022
-# Purpose: To implement minibatch gradient descent from scratch
+# Date: 3/20/2022
+# Purpose: To implement gradient descent from scratch
 
 from autograd import grad
 import autograd.numpy as np
 
 
-def minibatch(g, alpha, max_its, w, x, y, batch_size=32):
+def gradient_descent(g, alpha, max_its, w, x, y, flavor='batch', batch_size=32, 
+                     random_state=None):
     
-    """Finds optimal weights by using minibatch gradient descent
+    """Finds optimal weights by using gradient descent
        ---------------------------------------------------------
        
        Parameters:
@@ -18,13 +19,32 @@ def minibatch(g, alpha, max_its, w, x, y, batch_size=32):
             max_its: the maximum number of iteratiions to run gradient descent
             w: a numpy array of starting weights
             y: actual values
+            flavor: either batch or sgd
+            batch_size: the batch size if using stochastic gradient descent
+            random_state: a number to set for reproducibility
             
         Returns a tuple of the weight and cost histories"""
-    
-    # Take a random sample of the data
-    indices = np.random.randint(low=x.size, size=batch_size)
-    x_sample = x[:, indices]
-    y_sample = y[0, indices]
+        
+    # Safety check
+    if flavor not in ['batch', 'sgd']:
+        raise ValueError('flavor must be one of batch or sgd')
+        
+    # Set a seed
+    if random_state is not None:
+        np.random.RandomState(seed=random_state)
+        
+    # Perform gradient descent on the whole dataset
+    if flavor == 'batch':
+        x_sample = x
+        y_sample = y
+        
+    # SGD
+    else:
+        
+        # Take a random sample of the data
+        indices = np.random.randint(low=x.size, size=batch_size)
+        x_sample = x[:, indices]
+        y_sample = y[0, indices]
     
     # Weights to be updated
     wk = w
@@ -36,13 +56,15 @@ def minibatch(g, alpha, max_its, w, x, y, batch_size=32):
     cost_history = [g(w, x_sample, y_sample)]
     weight_history = [wk]
     
-    # Claculates weights by moving down the gradient
+    # Calculates weights by moving down the gradient
     for iter in range(max_its):
         
-        # Randomly sample the data
-        indices = np.random.randint(low=x.size, size=batch_size)
-        x_sample = x[:, indices]
-        y_sample = y[0, indices]
+        if flavor == 'sgd':
+            
+            # Randomly sample the data
+            indices = np.random.randint(low=x.size, size=batch_size)
+            x_sample = x[:, indices]
+            y_sample = y[0, indices]
         
         wk = wk - alpha*gradient(wk, x_sample, y_sample)
         cost_history.append(g(wk, x_sample, y_sample))
